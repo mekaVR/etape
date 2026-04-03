95ba07215b82178e95465d7dce412112f95df049
mekaVR <candau.herbert@gmail.com>
2026-04-03 10:38:39 +0200

    chore: setup husky pre-commit hook with lint-staged

--- apps/fongesoft/vite.config.ts
+++ apps/fongesoft/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import * as path from "node:path";
 
 // https://vite.dev/config/
@@ -14,4 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
