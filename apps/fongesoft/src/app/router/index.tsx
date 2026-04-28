b5945f6f54235d1911f9be616160e6d7695da248
meka VR <candau.herbert@gmail.com>
2026-04-28 14:01:14 +0200

    remove Supsense and Lazy

--- apps/fongesoft/src/app/router/index.tsx
+++ apps/fongesoft/src/app/router/index.tsx
@@ -1,21 +1,17 @@
-import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router";
 import { ProtectedRoute } from "@/app/router/protected-route.tsx";
-
-const LoginPage = lazy(() => import("@/features/auth/pages/login"));
-const SignupPage = lazy(() => import("@/features/auth/pages/signup"));
+import LoginPage from "@/features/auth/pages/login.tsx";
+import SignupPage from "@/features/auth/pages/signup.tsx";
 
 export function AppRouter() {
   return (
-    <Suspense fallback={null}>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
 
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<div />} />
-        </Route>
-      </Routes>
-    </Suspense>
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<div />} />
+      </Route>
+    </Routes>
   );
 }
--- apps/transition-pro/src/app/router/index.tsx
+++ apps/transition-pro/src/app/router/index.tsx
@@ -1,30 +1,26 @@
-import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router";
 import { ProtectedRoute } from "./protected-route";
 import Layout from "../layouts/layout";
-
-const LoginPage = lazy(() => import("@/features/auth/pages/login"));
-const SignupPage = lazy(() => import("@/features/auth/pages/signup"));
+import LoginPage from "@/features/auth/pages/login";
+import SignupPage from "@/features/auth/pages/signup.tsx";
 
 export function AppRouter() {
   return (
-    <Suspense fallback={null}>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route element={<ProtectedRoute />}>
-          <Route element={<Layout />}>
-            <Route path="/" element={<div>DASHBOARD</div>} />
-            <Route path="/messagerie" element={<div>MESSAGERIE</div>} />
-            <Route path="/profil" element={<div>PROFILE</div>} />
-            <Route path="/dossiers" element={<div>dossiers</div>} />
-            <Route path="/documents" element={<div>documents</div>} />
-            <Route path="/rendez-vous" element={<div>rendez-vous</div>} />
-            <Route path="/cpi" element={<div>cpi</div>} />
-            <Route path="/reseau" element={<div>reseau</div>} />
-          </Route>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route element={<Layout />}>
+          <Route path="/" element={<div>DASHBOARD</div>} />
+          <Route path="/messagerie" element={<div>MESSAGERIE</div>} />
+          <Route path="/profil" element={<div>PROFILE</div>} />
+          <Route path="/dossiers" element={<div>dossiers</div>} />
+          <Route path="/documents" element={<div>documents</div>} />
+          <Route path="/rendez-vous" element={<div>rendez-vous</div>} />
+          <Route path="/cpi" element={<div>cpi</div>} />
+          <Route path="/reseau" element={<div>reseau</div>} />
         </Route>
-      </Routes>
-    </Suspense>
+      </Route>
+    </Routes>
   );
 }
